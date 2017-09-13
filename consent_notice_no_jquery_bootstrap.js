381579f39ecc7fd8ff192c836960b0b4585e573f
Imran Akbar <iakbar@apple.com>
2017-09-13 16:25:36 -0700

    language updates

--- consent_notice_no_jquery_bootstrap.js
+++ consent_notice_no_jquery_bootstrap.js
@@ -23,20 +23,20 @@ function initialize_consent(token, privacy_url, text)
 		language = 'en';
 	}
 	
-	var language_texts = {en: "We use cookies on our site to improve your experience, according to our <a href='" + privacy_url + "'>privacy</a> policy.",
-		es:,//spanish
-		fr:"",//french
-		de:"",//german
-		pt:"",//portuguese
-		hi:"",//
-		ar:"",//arabic
-		zh:"",//chinese
-		ja:"",//japanese
-		ko:"",//korean
-		ru:"",//russian
-		it:"",//italian
-		ms:"",//malay
-		bn:""//bengali
+	var language_texts = {en: "We use cookies on our site to improve your experience, in accordance with our <a href='" + privacy_url + "'>privacy</a> policy.",
+		es:"Utilizamos cookies en nuestro sitio para mejorar su experiencia, de acuerdo con nuestra política de privacidad.",//spanish
+		fr:"Nous utilisons des cookies sur notre site pour améliorer votre expérience, conformément à notre politique de confidentialité.",//french
+		de:"Wir verwenden Cookies auf unserer Website, um Ihre Erfahrung zu verbessern, in Übereinstimmung mit unseren Datenschutzbestimmungen.",//german
+		pt:"Usamos cookies em nosso site para melhorar sua experiência, de acordo com nossa política de privacidade.",//portuguese
+		hi:"हमारी गोपनीयता नीति के अनुसार, हम आपके अनुभव को बेहतर बनाने के लिए हमारी साइट पर कुकीज़ का उपयोग करते हैं।",//hindi
+		ar:"نحن نستخدم ملفات تعريف الارتباط على موقعنا لتحسين تجربتك، وفقا لسياسة الخصوصية لدينا.",//arabic
+		zh:"我们根据我们的隐私政策，在我们的网站上使用Cookie来改善您的体验。",//chinese
+		ja:"私たちのプライバシーポリシーに従って、私たちはあなたの経験を向上させるために私たちのサイトでクッキーを使用します。",//japanese
+		ko:"당사는 당사의 개인 정보 취급 방침에 따라 귀하의 경험을 향상시키기 위해 당사 사이트의 쿠키를 사용합니다.",//korean
+		ru:"Мы используем файлы cookie на нашем сайте, чтобы улучшить ваш опыт в соответствии с нашей политикой конфиденциальности.",//russian
+		it:"Usiamo cookie sul nostro sito per migliorare la tua esperienza, in conformità con la nostra politica sulla privacy.",//italian
+		ms:"Kami menggunakan kuki di laman web kami untuk meningkatkan pengalaman anda, mengikut dasar privasi kami.",//malay
+		bn:"আমরা আমাদের গোপনীয়তা নীতি অনুযায়ী, আপনার অভিজ্ঞতা উন্নত করতে আমাদের সাইটে কুকিজ ব্যবহার"//bengali
 	}
 
 	if(language_texts.hasOwnProperty(language))
